e3d63dafa5c779cbebd0f648694cf6dd4d0169cd
Ryan Evans <139583451+RyanCodeGit@users.noreply.github.com>
2024-06-08 06:18:25 +0100

    adds loader function and connector class to db_utils.py

--- db_utils.py
+++ db_utils.py
@@ -1,3 +1,14 @@
+import yaml
+# Creating a function to load credentials from a YAML file
+def loader(filename):
+    file = open(filename, 'r') # specify file to be loaded
+    opened_file = yaml.safe_load(file) # loading the file previously specified
+    file.close() # closing file
+    return opened_file
+
+credentials = loader('credentials.yaml')
+
 # Creating a class to extract data from RDS database
 class RDSDatabaseConnector:
-    
+    def __init__(self, creds):
+        self.creds = creds
