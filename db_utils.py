592c2398f158e11c7d2141174c84a8e59fc1ce72
Ryan Evans <139583451+RyanCodeGit@users.noreply.github.com>
2024-06-10 13:13:12 +0100

    adds methods to RDSDatabaseConnector class in db_utils.py

--- db_utils.py
+++ db_utils.py
@@ -1,4 +1,8 @@
+import pandas as pd
+import psycopg2
 import yaml
+from sqlalchemy import create_engine
+
 # Creating a function to load credentials from a YAML file
 def loader(filename):
     file = open(filename, 'r') # specify file to be loaded
@@ -12,3 +16,20 @@ credentials = loader('credentials.yaml')
 class RDSDatabaseConnector:
     def __init__(self, creds):
         self.creds = creds
+
+    def start_sqlalchemy_engine(self):
+        self.engine = create_engine(f"postgresql+psycopg2://{self.creds['RDS_USER']}:{self.creds['RDS_PASSWORD']}@{self.creds['RDS_HOST']}:{self.creds['RDS_PORT']}/{self.creds['RDS_DATABASE']}")
+
+    def get_data(self, table_name):
+        self.df = pd.read_sql_table(f'{table_name}', self.engine)
+        return self.df
+
+    def export_to_csv(self, filename):
+        self.df.to_csv(f'{filename}.csv')
+
+# Testing methods below, will remove later
+
+# test = RDSDatabaseConnector(credentials)
+# test.start_sqlalchemy_engine()
+# test.get_data('loan_payments')
+# test.export_to_csv("loan payments")
